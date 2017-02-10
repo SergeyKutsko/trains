321aab509a6bcb2982c8ba437b0e6bc6f02b3eb3
Sergey Kutsko <kutsko86@gmail.com>
2017-02-10 12:11:20 +0200

    add #length_between #length_of_route functions to TrainRoutesAnalitics class

--- lib/train_routes_analitics.rb
+++ lib/train_routes_analitics.rb
@@ -1,19 +1,57 @@
+require_relative './train_routes_analitics/search_route_algorithms.rb'
 # Main class
 class TrainRoutesAnalitics
-  DEFAULT_ROUTE_REGEX = /[A-E]+\d+/
-  OUTPUT_FILE_NAME = 'out.txt'
+  VERTEX_REGEX = '[A-E]{2}'.freeze
+  DISTANCE_REGEX = '\d+'.freeze
+  DEFAULT_ROUTE_REGEX = Regexp.new "#{VERTEX_REGEX}#{DISTANCE_REGEX}"
+  OUTPUT_FILE_NAME = 'out.txt'.freeze
+  NO_ROUTE_MESSAGE = 'NO SUCH ROUTE'.freeze
 
-  attr_reader :input_data
+  include SearchRouteAlgorithms
 
-  def initialize(input_file_name, route_regexp = nil)
-    @route_regexp = route_regexp
+  attr_reader :input_data, :graph
+
+  def initialize(input_file_name, edge_regexp = nil)
+    @graph = Dijkstra::Graph.new
+    @distance_matrix = {}
+    @edge_regexp = edge_regexp
     @input_data = File.open(input_file_name) do |f|
-      f.read.scan(route_mask)
+      text = f.read
+      text.scan(route_mask)
+    end
+
+    build_graph
+  end
+
+  def length_between(v1, v2)
+    graph.length_between v1, v2
+  end
+
+  def length_of_route(path)
+    path = path.split('-')
+    current_vertex = path.shift
+    lengths = path.map do |next_vertex|
+      length = graph.length_between current_vertex, next_vertex
+      current_vertex = next_vertex
+      length
+    end
+    lengths.all? ? lengths.inject(&:+) : NO_ROUTE_MESSAGE
+  end
+
+  private
+
+  def build_graph
+    input_data.each do |edge|
+      v1, v2 = *edge.scan(/\p{Lu}/)
+      w = edge[/\d+/].to_i
+      graph.push v1
+      graph.push v2
+      graph.uniq!
+      graph.connect v1, v2, w
     end
   end
 
   def route_mask
-    @route_regexp || DEFAULT_ROUTE_REGEX
+    @edge_regexp || DEFAULT_ROUTE_REGEX
   end
-
 end
